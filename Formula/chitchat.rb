b0880ed197823b77c151020ad33a9f20c698b995
geordi <geordidearns@gmail.com>
2025-06-13 13:21:06 +0200

    Update SHA

--- Formula/chitchat.rb
+++ Formula/chitchat.rb
@@ -2,7 +2,7 @@ class Chitchat < Formula
   desc "Terminal-based chat tool"
   homepage "https://github.com/geordidearns/chitchat.sh"
   url "https://github.com/geordidearns/chitchat.sh/releases/tag/0.0.1"
-  sha256 "sha256:6a3fb1d5dce94c32e5ce8c9fbc18c96f0127e539a9f3a8a78b74666c2a80d498"
+  sha256 "98284e1fa749692fe75c0d88ebc36976aab46bc47c82f780a67a7286b6a57d5a"
   version "0.0.1"
 
   def install
