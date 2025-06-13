fe9a4f5a49f348009b328592cc7a453cceae0594
geordi <geordidearns@gmail.com>
2025-06-13 13:26:11 +0200

    Fix SHA256 checksum for binary

--- Formula/chitchat.rb
+++ Formula/chitchat.rb
@@ -2,7 +2,7 @@ class Chitchat < Formula
   desc "Terminal-based chat tool"
   homepage "https://github.com/geordidearns/chitchat.sh"
   url "https://github.com/geordidearns/chitchat.sh/releases/tag/0.0.1"
-  sha256 "98284e1fa749692fe75c0d88ebc36976aab46bc47c82f780a67a7286b6a57d5a"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "0.0.1"
 
   def install
